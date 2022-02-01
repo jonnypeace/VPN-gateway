70ef26108ec9a100fec8c3c699e1a72e1d72f5b4
Jonny <royalpython2@hotmail.com>
2022-02-01 16:59:33 +0000

    Update ipRes.sh

--- ipRes.sh
+++ ipRes.sh
@@ -2,9 +2,11 @@
 
 read -p "save firewall rules? [y/N] " ans
 
-if [ $ans = "y" ]; then
-iptables-save > /etc/iptables/rules.v4 ; else
-iptables-restore < /etc/iptables/rules.v4
+if [[ $ans = "y" ]]; then
+ iptables-save > /etc/iptables/rules.v4
+ echo "iptables saved"; else
+ iptables-restore < /etc/iptables/rules.v4
+ echo "iptables restored"
 fi
 # To add the new rules & without flushing current ones
 # sudo iptables-restore -n < /etc/iptables/rules.v4
